ce6e44f4c64b6515e4372c68ad24381ef5161782
irmmydn <irmmydn@gmail.com>
2023-12-25 23:44:51 +0300

    Update db.js

--- app_api/models/db.js
+++ app_api/models/db.js
@@ -1,6 +1,6 @@
 var mongoose  =require("mongoose");
 //var dbURI= "mongodb://127.0.0.1/mekanbul";
-var dbURI="mongodb+srv://iremmaydan:uLlkRN55rSCqZRLG@cluster0.iytchsd.mongodb.net/?retryWrites=true&w=majority/mekanbul"
+var dbURI="mongodb+srv://iremmaydan:uLlkRN55rSCqZRLG@cluster0.iytchsd.mongodb.net/mekanbul"
 mongoose.connect(dbURI);
 mongoose.connection.on("connected", function(){
     console.log("Bağlandı");
